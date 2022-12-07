d4e41506fdda0bc1d46eab9a24bed89379bb03b1
Sudhanshu Kumar <97729053+sudhanshusmith@users.noreply.github.com>
2022-12-07 14:43:14 +0530

    First Commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+var hamburger = document.querySelector(".hamburger");
+var navMenu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", function(){
+navMenu.classList.toggle("active");
+} );
